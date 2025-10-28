a3a98aaf85ef34b7f81948c703cb278d4e4f452a
くらりゅ！ <s1f102501260@iniad.org>
2025-10-28 15:04:16 +0900

    add gurd

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -2,12 +2,13 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { LoginDto } from './dto/login.dto';
-
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { UseGuards } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createAuthDto: CreateAuthDto) {
     return this.authService.create(createAuthDto);
--- backend/src/post/post.controller.ts
+++ backend/src/post/post.controller.ts
@@ -1,17 +1,20 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { PostService } from './post.service';
 import { CreatePostDto } from './dto/create-post.dto';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('post')
 // 認証を挟む
 export class PostController {
   constructor(private readonly postService: PostService) {}
-  
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createPostDto: CreatePostDto) {
     return this.postService.create(createPostDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.postService.findAll();
