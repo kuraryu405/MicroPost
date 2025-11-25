507ce30900c001cfa58420e46f7fb00517421264
倉野 <s1f102501260@iniad.org>
2025-11-25 12:30:08 +0900

    add env

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
